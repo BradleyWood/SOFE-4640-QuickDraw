dee4e8300935d3377206627c33e2331835b47678
BradleyWood <bradley.wood@uoit.net>
2018-11-28 21:51:01 -0500

    add info string to canvas

--- app/src/main/java/ca/uoit/quickdraw/view/QuickDrawCanvas.kt
+++ app/src/main/java/ca/uoit/quickdraw/view/QuickDrawCanvas.kt
@@ -15,24 +15,37 @@ class QuickDrawCanvas(ctx: Context, attrSet: AttributeSet) : View(ctx, attrSet) 
 
     private val strokes = LinkedList<Stroke>()
     private val paint: Paint = Paint()
+    private var infoText: String = ""
+    private var drawColor = Color.RED
     var strokeListener: StrokeListener? = null
 
     init {
-        paint.color = Color.BLACK
         paint.isAntiAlias = true
         paint.strokeWidth = 5f
         paint.style = Paint.Style.STROKE
+        paint.textSize = 40f
         paint.strokeJoin = Paint.Join.ROUND
         paint.strokeCap = Paint.Cap.ROUND
     }
 
     fun setDrawColor(color: Int) {
-        paint.color = color
+        drawColor = color
+    }
+
+    fun setInfoText(infoText: String) {
+        this.infoText = infoText
+        invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {
+        paint.color = Color.BLACK
+
+        canvas!!.drawText(infoText, width / 2f - 20f, 40f, paint)
+
+        paint.color = drawColor
+
         for (stroke in strokes) {
-            canvas!!.drawPath(stroke.path, paint)
+            canvas.drawPath(stroke.path, paint)
         }
     }
 
